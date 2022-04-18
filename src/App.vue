db727bbfff6a8b7f695b03518a45836ed497e233
clx <1149302926@qq.com>
2022-04-18 17:46:28 +0800

    路由和显示

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <router-view name="slideBar"></router-view>
+      <router-view name="main"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import PostList from "./components/PostList.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    PostList,
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: #e1e1e1;
+}
+.main {
+  margin: 0 auto;
+
+  width: 80%;
+}
+</style>
